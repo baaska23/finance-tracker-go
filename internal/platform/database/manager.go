657886db7fdb800d9f812568ac388d352186592e
kaitheengineer <93702715+baaska23@users.noreply.github.com>
2026-03-16 08:09:50 +0800

    DATABASE_URL=postgresql://neondb_owner:npg_NXiJSE2Y8KpO@ep-soft-dream-a1nuevqg-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require

--- internal/platform/database/manager.go
+++ internal/platform/database/manager.go
@@ -1,9 +1,24 @@
 package database
 
-import "database/sql"
+import (
+	"context"
+	"log"
+	"os"
 
-type DbManager struct {
-	RTV *sql.DB
-	Billing *sql.DB
-	Auth *sql.DB
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading env file")
+    }
+    connStr := os.Getenv("DATABASE_URL")
+    conn, connErr := pgx.Connect(context.Background(), connStr)
+    if connErr != nil {
+        log.Fatal("Unable to connect to database:", connErr)
+    }
+    defer conn.Close(context.Background())
+    log.Println("Successfully connected")
 }
---
+++ note.md
@@ -0,0 +1,12 @@
+entity Transaction
+dto CreateTransaction, UpdateTransaction
+
+
+Transaction
+transaction_id
+date
+amount
+category
+note
+created_at
+updated_at
